2a54e4dbf9abbf499fd6c04073d3651377d92ab3
Lijiang Long <llong35@gatech.edu>
2019-10-28 22:41:19 -0400

    add to be able to use new torch

--- validation.py
+++ validation.py
@@ -50,7 +50,7 @@ def val_epoch(epoch, data_loader, model, criterion, opt, logger):
                 confusion_matrix[rows[idx]][columns[idx]] +=1
             '''
             ###########################################################################
-            losses.update(loss.data[0], inputs.size(0))
+            losses.update(loss.item(), inputs.size(0))
             accuracies.update(acc, inputs.size(0))
 
             batch_time.update(time.time() - end_time)
