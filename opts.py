3009e772971588ba5c7a22974c3d701bf7f34f9f
Lijiang Long <llong35@gatech.edu>
2020-02-22 22:50:19 -0500

    find bottlenect

--- opts.py
+++ opts.py
@@ -108,7 +108,7 @@ def parse_opts():
         '--batch_size', default=12, type=int, help='Batch Size')
     parser.add_argument(
         '--n_epochs',
-        default=100,
+        default=1,
         type=int,
         help='Number of total epochs to run')
     parser.add_argument(
